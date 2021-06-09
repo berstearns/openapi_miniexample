8b021227089c713199ca4b879823523196650813
berstearns <bernardo.stearns@gmail.com>
2021-06-09 12:01:48 -0300

    cde

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ app = Flask(__name__)
 @app.route('/foo/<language>', methods=['POST']) 
 def foo(language):
     corpus = request.json
+    corpus.update({"language":language})
     return jsonify(corpus)
+
+
+if __name__ == "__main__":
+    app.run()
